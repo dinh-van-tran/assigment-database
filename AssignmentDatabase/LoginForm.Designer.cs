ddbdd9aea9f03e6d1f85fffa335b821ad9d20694
haiprotn <haiprotn@gmail.com>
2019-04-28 16:49:36 +0700

    tao form thiet bi

--- AssignmentDatabase/LoginForm.Designer.cs
+++ AssignmentDatabase/LoginForm.Designer.cs
@@ -83,6 +83,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(541, 252);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
