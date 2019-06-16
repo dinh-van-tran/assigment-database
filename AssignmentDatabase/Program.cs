e98586f47910e76c9ca318fa87d4ccffb6a7e13d
dinh-tran <tranvan538@gmail.com>
2019-06-16 13:33:15 +0700

    update

--- AssignmentDatabase/Program.cs
+++ AssignmentDatabase/Program.cs
@@ -17,7 +17,7 @@ namespace AssignmentDatabase
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ActionForm());
+            Application.Run(new LoginForm());
         }
     }
 }
